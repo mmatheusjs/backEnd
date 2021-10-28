2764cea743b6fafd7de31d5f4970c56e3a98c76b
mmatheusjs <math.heenrique@live.com>
2021-10-27 22:37:39 -0300

    Creation of route of delete users

--- src/routes/UsersRouter.js
+++ src/routes/UsersRouter.js
@@ -1,93 +1,119 @@
-import { Router } from "express";
-import { getManager } from "typeorm";
+import { Router } from 'express'
+import { getManager } from 'typeorm'
 
-const usersRouter = Router();
+const usersRouter = Router()
 
-usersRouter.get("/", async function (req, res) {
-  const entityManager = getManager();
+usersRouter.get('/', async function (req, res) {
+  const entityManager = getManager()
 
-  const response = await entityManager.query(`SELECT * from users `);
+  const response = await entityManager.query(`SELECT * from users `)
 
-  res.json(response);
-});
+  res.json(response)
+})
 
-usersRouter.post("/", async function (req, res) {
-  const { email, password, name, telephone } = req.body;
+usersRouter.post('/', async function (req, res) {
+  const { email, password, name, telephone } = req.body
 
   if (email === undefined) {
     return res.status(401).json({
-      message: "Campo Email não encontrado",
-    });
+      message: 'Campo Email não encontrado'
+    })
   }
 
   if (name === undefined) {
     return res.status(401).json({
-      message: "Campo Nome não encontrado",
-    });
+      message: 'Campo Nome não encontrado'
+    })
   }
 
   if (password === undefined) {
     return res.status(401).json({
-      message: "Campo Senha não encontrado",
-    });
+      message: 'Campo Senha não encontrado'
+    })
   }
 
   if (telephone === undefined) {
     return res.status(401).json({
-      message: "Campo Telefone não encontrado",
-    });
+      message: 'Campo Telefone não encontrado'
+    })
   }
 
-  const entityManager = getManager();
+  const entityManager = getManager()
 
   const response = await entityManager.query(
     `SELECT * from users where email like '${email}' ;`
-  );
+  )
 
   if (response.length === 0) {
     await entityManager.query(
       `INSERT INTO users (name, email, password, telephone) 
       VALUES ('${name}', '${email}', '${password}', '${telephone}' );`
-    );
+    )
 
     return res.status(200).json({
-      message: "Usuario Criado",
-    });
+      message: 'Usuario Criado'
+    })
   } else {
     return res.status(401).json({
-      message: "Email Já cadastrado",
-    });
+      message: 'Email Já cadastrado'
+    })
   }
-});
+})
 
-usersRouter.post("/login", async function (req, res) {
-  const { email, password } = req.body;
+usersRouter.post('/delete', async function (req, res) {
+  const { id } = req.body
+
+  if (id === undefined) {
+    return res.status(401).json({
+      message: 'Campo ID não encontrado'
+    })
+  }
+
+  const entityManager = getManager()
+
+  const response = await entityManager.query(
+    `DELETE FROM users where id = ${id} ;`
+  )
+
+  if (response[1] === 0) {
+    return res.status(401).json({
+      message: 'Usuário não existe'
+    })
+  } else {
+    return res.status(200).json({
+      message: 'Usuário deletado'
+    })
+  }
+})
+
+usersRouter.post('/login', async function (req, res) {
+  const { email, password } = req.body
 
   if (email === undefined) {
     return res.status(401).json({
-      message: "Campo Email não encontrado",
-    });
+      message: 'Campo Email não encontrado'
+    })
   }
 
   if (password === undefined) {
     return res.status(401).json({
-      message: "Campo Senha não encontrado",
-    });
+      message: 'Campo Senha não encontrado'
+    })
   }
 
-  const entityManager = getManager();
+  const entityManager = getManager()
 
   const response = await entityManager.query(
     `SELECT * from users where email like '${email}' and password like '${password}' ;`
-  );
+  )
 
   if (response.length === 0) {
     return res.status(401).json({
-      message: "Email ou senha invalidos",
-    });
+      message: 'Email ou senha invalidos'
+    })
   }
 
-  res.json(response);
-});
+  res.json(response)
+})
 
-export default usersRouter;
+export default usersRouter

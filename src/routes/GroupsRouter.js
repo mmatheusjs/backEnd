c296b0d2180ae40445268577759fe72ff1878954
mmatheusjs <math.heenrique@live.com>
2021-12-08 22:16:05 -0300

    Changes at groupsRouter, insert in table scheduling and group_mass

--- src/routes/GroupsRouter.js
+++ src/routes/GroupsRouter.js
@@ -13,13 +13,69 @@ groupsRouter.post('/', async function (req, res) {
     })
   }
 
-  const response = await entityManager.query(
-    `SELECT * from group_mass WHERE manager_id = ${id}`
+  if (week_of_month === undefined || week_of_month.length > 2) {
+    return res.status(401).json({
+      message: 'Selecione apenas duas semanas'
+    })
+  }
+
+  if (day_of_week === undefined) {
+    return res.status(401).json({
+      message: 'Selecione o dia e o horário'
+    })
+  }
+
+  if (time_schedule === undefined) {
+    return res.status(401).json({
+      message: 'Selecione o dia e o horário'
+    })
+  }
+
+  let groupMassId = null
+
+  const groupMassResponse = await entityManager.query(
+    `SELECT id from group_mass WHERE manager_id = ${id}`
   )
 
-  console.log(response)
+  if (groupMassResponse.length === 0) {
+    const createGroupResponse = await entityManager.query(
+      `INSERT INTO group_mass (manager_id) 
+      VALUES (${id});`
+    )
 
-  res.json(response)
+    const groupMass = await entityManager.query(
+      `SELECT id from group_mass WHERE manager_id = ${id}`
+    )
+    groupMassId = groupMass[0].id
+  } else groupMassId = groupMassResponse[0].id
+
+  const schedulingResponse = await entityManager.query(
+    `SELECT id from scheduling WHERE group_id = ${groupMassId}`
+  )
+
+  if (schedulingResponse.length > 1) {
+    return res.status(401).json({
+      message: 'Você só pode ter dois horários cadastrados'
+    })
+  }
+  week_of_month.forEach(async week => {
+    const searchSchedulingResponse = await entityManager.query(
+      `SELECT id from scheduling WHERE day_of_week like '${day_of_week}' and time_schedule like '${time_schedule}' and  week_of_month like '${week}'`
+    )
+
+    if (searchSchedulingResponse.length === 0) {
+      const insertSchedulingResponse = await entityManager.query(
+        `INSERT INTO scheduling (day_of_week, time_schedule, week_of_month, group_id ) 
+        VALUES ('${day_of_week}', '${time_schedule}', '${week}', ${groupMassId} );`
+      )
+    } else {
+      return res.status(401).json({
+        message: 'Esse horário já está reservado'
+      })
+    }
+  })
+
+  res.json({ message: 'Agendamento concluído' })
 })
 
 export default groupsRouter
